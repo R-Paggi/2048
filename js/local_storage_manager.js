f24b50a2396750541f9233ed749460e2563ef983
Sauli Bueno <sauli@precisasoftware.com>
2026-06-03 14:52:42 -0300

    feat: adicionar seleção de tamanho de tabuleiro 3x3, 4x4 e 5x5

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -18,7 +18,7 @@ window.fakeStorage = {
   }
 };
 
-function LocalStorageManager1() {
+function LocalStorageManager() {
   this.bestScoreKey     = "bestScore";
   this.gameStateKey     = "gameState";
 
@@ -26,7 +26,7 @@ function LocalStorageManager1() {
   this.storage = supported ? window.localStorage : window.fakeStorage;
 }
 
-LocalStorageManager1.prototype.localStorageSupported = function () {
+LocalStorageManager.prototype.localStorageSupported = function () {
   var testKey = "test";
 
   try {
@@ -40,24 +40,24 @@ LocalStorageManager1.prototype.localStorageSupported = function () {
 };
 
 // Best score getters/setters
-LocalStorageManager1.prototype.getBestScore = function () {
+LocalStorageManager.prototype.getBestScore = function () {
   return this.storage.getItem(this.bestScoreKey) || 0;
 };
 
-LocalStorageManager1.prototype.setBestScore = function (score) {
+LocalStorageManager.prototype.setBestScore = function (score) {
   this.storage.setItem(this.bestScoreKey, score);
 };
 
 // Game state getters/setters and clearing
-LocalStorageManager1.prototype.getGameState = function () {
+LocalStorageManager.prototype.getGameState = function () {
   var stateJSON = this.storage.getItem(this.gameStateKey);
   return stateJSON ? JSON.parse(stateJSON) : null;
 };
 
-LocalStorageManager1.prototype.setGameState = function (gameState) {
+LocalStorageManager.prototype.setGameState = function (gameState) {
   this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
 };
 
-LocalStorageManager1.prototype.clearGameState = function () {
+LocalStorageManager.prototype.clearGameState = function () {
   this.storage.removeItem(this.gameStateKey);
 };
--- js/tile.js
+++ js/tile.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-function Tile(position, value) {
-  this.x                = position.x;
-  this.y                = position.y;
-  this.value            = value || 2;
-
-  this.previousPosition = null;
-  this.mergedFrom       = null; // Tracks tiles that merged together
-}
-
-Tile.prototype.savePosition = function () {
-  this.previousPosition = { x: this.x, y: this.y };
-};
-
-Tile.prototype.updatePosition = function (position) {
-  this.x = position.x;
-  this.y = position.y;
-};
-
-Tile.prototype.serialize = function () {
-  return {
-    position: {
-      x: this.x,
-      y: this.y
-    },
-    value: this.value
-  };
-};
-=======
 function Tile(position, value, type) {
   this.x                = position.x;
   this.y                = position.y;
@@ -56,4 +27,3 @@ Tile.prototype.serialize = function () {
     type: this.type
   };
 };
->>>>>>> 2600a93d712238063fd72d6bfd56adc3734800e5
